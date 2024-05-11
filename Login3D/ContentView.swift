8006004f45769fad993bbbedaddf4e4e43060d42
mikle <beardmikle@gmail.com>
2024-05-11 16:53:51 +0300

    Initial Commit

---
+++ Login3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Login3D
+//
+//  Created by beardmikle on 11.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Login3D/Login3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login3DApp.swift
+//  Login3D
+//
+//  Created by beardmikle on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Login3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
