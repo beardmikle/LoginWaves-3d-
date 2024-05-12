3e58ac4c27b7f40fb84cb64f7c45343b93549eb3
mikle <beardmikle@gmail.com>
2024-05-13 01:17:43 +0300

    1

---
+++ Login3D/Waves3D.swift
@@ -0,0 +1,18 @@
+//
+//  Waves3D.swift
+//  Login3D
+//
+//  Created by beardmikle on 11.05.2024.
+//
+
+import SwiftUI
+
+struct Waves3D: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Waves3D()
+}
